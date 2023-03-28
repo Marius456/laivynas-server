af577fa27bf7d391d2d7092ab549c1fe7bdebe8c
Marius456 <47567608+Marius456@users.noreply.github.com>
2023-03-28 14:26:18 +0300

    tutorial stage

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.set('view engine', 'ejs')
+
+const userRouter = require("./routes/users")
+
+app.use('/users', userRouter)
+
+function logger(req, res, next){
+    console.log(req.originalUrl)
+    next()
+}
+
 app.listen(3003)
